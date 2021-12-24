62eee250bbb920c893dc04e17fe70d5d4d4d8771
yongboshim <94089402+yongbospace@users.noreply.github.com>
2021-12-24 10:38:35 +0900

    http -> https

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,7 @@ const API_KEY = "7a042a8156a85d216f3bd65bc90bdf7d";
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
